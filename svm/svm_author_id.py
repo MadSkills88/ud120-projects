87fcef306ab80f5e3f3170615ff5169f97331cf8
Anthony Bao <anthonybao1999@gmail.com>
2017-05-08 12:19:17 -0700

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,8 +24,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-# features_train = features_train[:len(features_train)/100] 
-# labels_train = labels_train[:len(labels_train)/100] 
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
 
 from sklearn.svm import SVC
 clf = SVC(C=10000, kernel="rbf")
@@ -38,6 +38,14 @@ print "prediction time:", round(time()-t0, 3), "s"
 from sklearn.metrics import accuracy_score
 accuracy = accuracy_score(labels_pred, labels_test)
 print accuracy
+
+answer1 = labels_pred(10)
+print answer1
+answer2 = labels_pred(26)
+print answer2
+answer3 = labels_pred(50)
+print answer3
+
 #########################################################
 
 
