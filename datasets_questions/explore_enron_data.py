96b985d8b37dc412234d2f8af6761ba79d026924
Anthony Bao <anthonybao1999@gmail.com>
2017-05-10 11:38:03 -0700

    Update explore_enron_data.py

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,8 +18,5 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
-num_people = 0
-for person in enron_data:
-    num_people += 1
-print "number of people: ", num_people
+print "number of people: ", len(enron_data)
 print "number of features: ", len(enron_data[0])
