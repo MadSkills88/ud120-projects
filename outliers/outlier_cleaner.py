a0be4989163ac2b778b006f193648881a2d4802c
Anthony Bao <anthonybao1999@gmail.com>
2017-05-11 17:08:38 -0700

    Update outlier_cleaner.py

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,7 +14,23 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
-
-    
+    residual_error_list = []
+    residual_error_list_copy = []
+    outliers_indices = []
+    outliers_error = []
+    final_count = len(predictions)*0.1
+    for i in range(len(predictions)):
+        error = predictions[i]-net_worths[i]
+        if error<0: error *= -1
+        residual_error_list.append(error)
+        residual_error_list_copy = residual_error_list
+    for j in range(final_count):
+        outliers_error.append(max(residual_error_list))
+        max_index = residual_error_list.index(max(residual_error_list))
+        outliers_indices.append(max_index)
+        residual_error_list.remove(max(residual_error_list))
+    for k in range(len(residual_error_list)):
+        if k not in outliers_indices:
+            cleaned_data.append(predictions[k], ages[k], net_worths[k])
     return cleaned_data
 
